cddc9ad3eecb56909486e48882d9f3a6dacaafd6
Sohail Rehman <sohailmalikraja@gmail.com>
2023-05-23 15:28:41 +0530

    added logic in js

---
+++ logic.js
@@ -0,0 +1,20 @@
+function appendNumber(number) {
+    document.getElementById("result").value += number;
+  }
+
+  function appendOperator(operator) {
+    document.getElementById("result").value += operator;
+  }
+
+  function calculate() {
+    try {
+      var result = eval(document.getElementById("result").value);
+      document.getElementById("result").value = result;
+    } catch (error) {
+      document.getElementById("result").value = "Error";
+    }
+  }
+
+  function clearResult() {
+    document.getElementById("result").value = "";
+  }
