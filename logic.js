4f55a590bc0e09a4142d01906e354ba32cb2414a
Sohail Rehman <sohailmalikraja@gmail.com>
2023-08-16 09:48:55 +0530

    display complete

--- logic.js
+++ logic.js
@@ -1,20 +1,30 @@
-function appendNumber(number) {
-    document.getElementById("result").value += number;
-  }
+const add =(n1,n2)=>n1+n2
+const subtract =(n1,n2)=>n1-n2
+const multiply =(n1,n2)=>n1*n2
+const divide =(n1,n2)=>n1/n2
 
-  function appendOperator(operator) {
-    document.getElementById("result").value += operator;
-  }
+function operate(n1,n2,op){
+  if(op=='+')return add(n1,n2)
+  else if(op=='-')return subtract(n1,n2)
+  else if(op=='*')return multiply(n1,n2)
+  else if(op=='/')return divide (n1,n2)
+}
 
-  function calculate() {
-    try {
-      var result = eval(document.getElementById("result").value);
-      document.getElementById("result").value = result;
-    } catch (error) {
-      document.getElementById("result").value = "Error";
-    }
-  }
-
-  function clearResult() {
-    document.getElementById("result").value = "";
-  }
+//clear button working
+const allclear =document.querySelector('#all-clear')
+const temp = document.querySelector('#tmp')
+const input = document.querySelector('#input')
+allclear.addEventListener('click',()=>{
+    temp.innerText=''
+    input.innerText=''
+})
+//get numbers
+let n1=''
+const numbers =document.querySelectorAll('.number')
+const numsarr = Array.prototype.slice.call(numbers);
+numsarr.forEach(number=>{
+  number.addEventListener('click',e=>{
+    let text=e.target.innerText
+    console.log(text)
+  })
+})
